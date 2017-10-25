8e79fe1aad443d0dc9874d5a6ac9600a30ac76f6
Concetto Caruso <caruso.jean@gmail.com>
2017-10-25 23:22:39 +0200

    first import

---
+++ check_unread.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import imaplib
+import credential
+
+obj = imaplib.IMAP4_SSL('imap.gmail.com')
+obj.login(credential.login, credential.password)
+obj.select()
+
+print ('hello')
+
+print (len(obj.search(None, 'UnSeen')[1][0].split()))
